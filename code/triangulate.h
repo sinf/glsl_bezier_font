2785ea0622926bfb1795e85da9c6a815d83d8152
ArhoM <arho.iivari@elisanet.fi>
2014-02-09 22:34:43 +0200

    Correctly rasterized solid glyphs, FINALLY! (certain glyphs have vertex winding issues though) Added a new dependency, GLU (used by triangulate.c for filling glyph interiors) Removed a whole bunch of stuff from triangulate.c and made some assumptions Fixed the bug that made all glyphs look like Comic Sans font_shader.c/draw_glyphs() accepts now flags that specify how to draw font_layout.c: implemented a simple layout function (horizontal & monospace) main.c: small changes all over the file linkedlist.c: did I even change anything? bezv.glsl: tells the fragment shader which color to use. can make off-curve points look different bezf.glsl: removed some logic

--- code/triangulate.h
+++ code/triangulate.h
@@ -1,15 +1,32 @@
 #ifndef _TRIANGULATE_H
 #define _TRIANGULATE_H
 #include "font_data.h"
+#include "types.h"
 
+/* memory limits */
 enum {
-	/* memory limits */
-	MAX_CONTOURS = 128, /* max contours per glyph (FreeMono.ttf has 120) */
-	MAX_HOLES = 32, /* max holes per polygon */
-	MAX_POINTS = 512 /* max points per contour (FreeMono.ttf has 480) */
+	MAX_GLYPH_CONTOURS = 128, /* Max contours per glyph (FreeMono.ttf has 120) */
+	MAX_GLYPH_POINTS = 1024, /* Max total per glyph (FreeMono.ttf has up to 480). Includes generated points */
+	MAX_GLYPH_TRI_INDICES = 2048, /* Max triangle indices */
+	MAX_POLYGON_HOLES = 32 /* Max nested contours */
 };
 
+/* error codes */
+typedef enum {
+	TR_SUCCESS=0,
+	TR_POINTS_LIMIT, /* too many points */
+	TR_HOLES_LIMIT, /* too many nested contours */
+	TR_INDICES_LIMIT, /* too many indices */
+	TR_ALLOC_FAIL, /* calloc/malloc failed */
+	TR_EMPTY_CONTOUR, /* a contour has no points */
+	TR_NONE_ON_CURVE /* a contour does not have any on-curve points */
+} TrError;
+
 /* Returns 0 on failure */
-int triangulate_contours( GlyphTriangles gt[1], uint8 point_flags[MAX_POINTS], float points[2*MAX_POINTS], uint16 end_points[], uint32 num_contours );
+TrError triangulate_contours( GlyphTriangles gt[1],
+	PointFlag point_flags[MAX_GLYPH_POINTS],
+	PointCoord point_coords[2*MAX_GLYPH_POINTS],
+	uint16 end_points[],
+	uint32 num_contours );
 
 #endif
